caa4215cc123c8838354380a7b3c6f39fd8cc1bd
Nazareno Medina <nmedina@nmedina-m2.local>
2017-02-07 16:35:41 -0600

    Initial commit

---
+++ src/main/java/com/schedder/service/player/PlayerserviceApplication.java
@@ -0,0 +1,12 @@
+package com.schedder.service.player;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayerserviceApplication.class, args);
+	}
+}
